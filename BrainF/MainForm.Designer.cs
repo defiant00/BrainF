1d4d01951a6e254a2a6206db68d573d756c72153
Aaron Mady <aaron.mady@amec.com>
2015-11-16 18:01:44 -0500

    interpreter and most of the compiler done

--- BrainF/MainForm.Designer.cs
+++ BrainF/MainForm.Designer.cs
@@ -28,30 +28,33 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.textBoxInput = new System.Windows.Forms.TextBox();
+			this.textBoxCode = new System.Windows.Forms.TextBox();
 			this.buttonInterpret = new System.Windows.Forms.Button();
 			this.buttonBuild = new System.Windows.Forms.Button();
 			this.labelOutput = new System.Windows.Forms.Label();
+			this.textBoxInput = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
-			// textBoxInput
+			// textBoxCode
 			// 
-			this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.textBoxCode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.textBoxInput.Location = new System.Drawing.Point(8, 8);
-			this.textBoxInput.Multiline = true;
-			this.textBoxInput.Name = "textBoxInput";
-			this.textBoxInput.Size = new System.Drawing.Size(608, 392);
-			this.textBoxInput.TabIndex = 0;
+			this.textBoxCode.Location = new System.Drawing.Point(8, 8);
+			this.textBoxCode.Multiline = true;
+			this.textBoxCode.Name = "textBoxCode";
+			this.textBoxCode.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBoxCode.Size = new System.Drawing.Size(608, 360);
+			this.textBoxCode.TabIndex = 0;
 			// 
 			// buttonInterpret
 			// 
 			this.buttonInterpret.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonInterpret.Location = new System.Drawing.Point(448, 408);
+			this.buttonInterpret.Location = new System.Drawing.Point(448, 400);
 			this.buttonInterpret.Name = "buttonInterpret";
 			this.buttonInterpret.Size = new System.Drawing.Size(80, 32);
-			this.buttonInterpret.TabIndex = 1;
+			this.buttonInterpret.TabIndex = 2;
 			this.buttonInterpret.Text = "Interpret";
 			this.buttonInterpret.UseVisualStyleBackColor = true;
 			this.buttonInterpret.Click += new System.EventHandler(this.buttonInterpret_Click);
@@ -59,10 +62,10 @@
 			// buttonBuild
 			// 
 			this.buttonBuild.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonBuild.Location = new System.Drawing.Point(536, 408);
+			this.buttonBuild.Location = new System.Drawing.Point(536, 400);
 			this.buttonBuild.Name = "buttonBuild";
 			this.buttonBuild.Size = new System.Drawing.Size(80, 32);
-			this.buttonBuild.TabIndex = 2;
+			this.buttonBuild.TabIndex = 3;
 			this.buttonBuild.Text = "Build";
 			this.buttonBuild.UseVisualStyleBackColor = true;
 			this.buttonBuild.Click += new System.EventHandler(this.buttonBuild_Click);
@@ -72,22 +75,42 @@
 			this.labelOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.labelOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.labelOutput.Location = new System.Drawing.Point(8, 408);
+			this.labelOutput.Location = new System.Drawing.Point(8, 400);
 			this.labelOutput.Name = "labelOutput";
 			this.labelOutput.Size = new System.Drawing.Size(432, 32);
-			this.labelOutput.TabIndex = 3;
-			this.labelOutput.Text = "test";
+			this.labelOutput.TabIndex = 4;
 			this.labelOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// textBoxInput
+			// 
+			this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxInput.Location = new System.Drawing.Point(48, 376);
+			this.textBoxInput.Name = "textBoxInput";
+			this.textBoxInput.Size = new System.Drawing.Size(568, 20);
+			this.textBoxInput.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(8, 376);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(34, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Input:";
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(624, 442);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBoxInput);
 			this.Controls.Add(this.labelOutput);
 			this.Controls.Add(this.buttonBuild);
 			this.Controls.Add(this.buttonInterpret);
-			this.Controls.Add(this.textBoxInput);
+			this.Controls.Add(this.textBoxCode);
 			this.MinimumSize = new System.Drawing.Size(640, 480);
 			this.Name = "MainForm";
 			this.Text = "BrainF";
@@ -98,10 +121,12 @@
 
 		#endregion
 
-		private System.Windows.Forms.TextBox textBoxInput;
+		private System.Windows.Forms.TextBox textBoxCode;
 		private System.Windows.Forms.Button buttonInterpret;
 		private System.Windows.Forms.Button buttonBuild;
 		private System.Windows.Forms.Label labelOutput;
+		private System.Windows.Forms.TextBox textBoxInput;
+		private System.Windows.Forms.Label label1;
 	}
 }
 
--- BrainF/MainForm.cs
+++ BrainF/MainForm.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BrainF
 {
 	public partial class MainForm : Form
 	{
+		public string Code { get { return textBoxCode.Text; } }
+		public string Input { get { return textBoxInput.Text; } }
+		public string Output { set { labelOutput.Text = value; } }
+
+		private const string Commands = "<>+-.,[]";
+
 		public MainForm()
 		{
 			InitializeComponent();
@@ -19,12 +22,215 @@ namespace BrainF
 
 		private void buttonInterpret_Click(object sender, EventArgs e)
 		{
+			var output = new StringBuilder();
+			var sb = new StringBuilder();
+			foreach (var c in Code) { if (Commands.IndexOf(c) > -1) { sb.Append(c); } }
+			string code = sb.ToString();
+			string input = Input;
 
+			int ip = 0, dp = 0;
+			byte[] data = new byte[30000];
+
+			while (ip < code.Length)
+			{
+				switch (code[ip++])
+				{
+					case '>':
+						dp++;
+						break;
+					case '<':
+						dp--;
+						break;
+					case '+':
+						data[dp]++;
+						break;
+					case '-':
+						data[dp]--;
+						break;
+					case '.':
+						output.Append((char)data[dp]);
+						break;
+					case ',':
+						if (input.Length > 0)
+						{
+							data[dp] = (byte)input[0];
+							input = input.Substring(1);
+						}
+						break;
+					case '[':
+						if (data[dp] == 0)
+						{
+							// Jump forward
+							int numOpen = 1;
+							while (numOpen > 0 && ip < code.Length)
+							{
+								switch (code[ip++])
+								{
+									case '[':
+										numOpen++;
+										break;
+									case ']':
+										numOpen--;
+										break;
+								}
+							}
+						}
+						break;
+					case ']':
+						if (data[dp] != 0)
+						{
+							// Jump back
+							ip -= 2;
+							int numOpen = 1;
+							while (numOpen > 0 && ip >= 0)
+							{
+								switch (code[ip--])
+								{
+									case '[':
+										numOpen--;
+										break;
+									case ']':
+										numOpen++;
+										break;
+								}
+							}
+							ip += 2; // Move to the command after the [
+						}
+						break;
+				}
+			}
+
+			Output = output.ToString();
 		}
 
 		private void buttonBuild_Click(object sender, EventArgs e)
 		{
+			var writeMethod = typeof(Console).GetMethod("Write", new[] { typeof(char) });
+			var strLenMethod = typeof(string).GetProperty("Length").GetGetMethod();
+			var strCharsMethod = typeof(string).GetProperty("Chars").GetGetMethod();
+			var substringMethod = typeof(string).GetMethod("Substring", new[] { typeof(int) });
 
+			var OpenLabels = new Stack<System.Reflection.Emit.Label>();
+			var CloseLabels = new Stack<System.Reflection.Emit.Label>();
+
+			string name = "CompiledTest";
+			var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName(name), AssemblyBuilderAccess.Save);
+			var moduleBuilder = assemblyBuilder.DefineDynamicModule(name + ".exe", true);
+			var typeBuilder = moduleBuilder.DefineType("Program", TypeAttributes.Public);
+			var main = typeBuilder.DefineMethod("Main", MethodAttributes.Private | MethodAttributes.HideBySig | MethodAttributes.Static, typeof(void),
+				new[] { typeof(string[]) });
+			main.DefineParameter(1, ParameterAttributes.None, "args");
+			var il = main.GetILGenerator();
+
+			il.DeclareLocal(typeof(byte[])).SetLocalSymInfo("data");
+			il.DeclareLocal(typeof(int)).SetLocalSymInfo("dp");
+			il.DeclareLocal(typeof(string)).SetLocalSymInfo("input");
+
+			// data = new byte[30000]
+			il.Emit(OpCodes.Ldc_I4, 30000);
+			il.Emit(OpCodes.Newarr, typeof(byte));
+			il.Emit(OpCodes.Stloc_0);
+			// dp = 0
+			il.Emit(OpCodes.Ldc_I4_0);
+			il.Emit(OpCodes.Stloc_1);
+			// input = ""
+			il.Emit(OpCodes.Ldstr, "");
+			il.Emit(OpCodes.Stloc_2);
+			// if (args.Length > 0) input = args[0]
+			il.Emit(OpCodes.Ldarg_0);
+			il.Emit(OpCodes.Ldlen);
+			il.Emit(OpCodes.Ldc_I4_0);
+			il.Emit(OpCodes.Cgt_Un);
+			var noArgsLbl = il.DefineLabel();
+			il.Emit(OpCodes.Brfalse_S, noArgsLbl);
+			il.Emit(OpCodes.Ldarg_0);
+			il.Emit(OpCodes.Ldc_I4_0);
+			il.Emit(OpCodes.Ldelem_Ref);
+			il.Emit(OpCodes.Stloc_2);
+			il.MarkLabel(noArgsLbl);
+
+			foreach (var c in Code)
+			{
+				switch (c)
+				{
+					case '>':
+					case '<':
+						EmitLeftRight(il, c == '>');
+						break;
+					case '+':
+					case '-':
+						EmitPlusMinus(il, c == '+');
+						break;
+					case '.':
+						EmitOutput(il, writeMethod);
+						break;
+					case ',':
+						EmitInput(il, strLenMethod, strCharsMethod, substringMethod);
+						break;
+					case '[':
+						break;
+					case ']':
+						break;
+				}
+			}
+
+			il.EmitWriteLine("");
+			il.Emit(OpCodes.Ret);
+			assemblyBuilder.SetEntryPoint(main);
+			typeBuilder.CreateType();
+			assemblyBuilder.Save(name + ".exe");
+			MessageBox.Show("Done");
+		}
+
+		private static void EmitLeftRight(ILGenerator il, bool right)
+		{
+			il.Emit(OpCodes.Ldloc_1);
+			il.Emit(OpCodes.Ldc_I4_1);
+			il.Emit(right ? OpCodes.Add : OpCodes.Sub);
+			il.Emit(OpCodes.Stloc_1);
+		}
+
+		private static void EmitPlusMinus(ILGenerator il, bool plus)
+		{
+			il.Emit(OpCodes.Ldloc_0);
+			il.Emit(OpCodes.Ldloc_1);
+			il.Emit(OpCodes.Ldloc_0);
+			il.Emit(OpCodes.Ldloc_1);
+			il.Emit(OpCodes.Ldelem_U1);
+			il.Emit(OpCodes.Ldc_I4_1);
+			il.Emit(plus ? OpCodes.Add : OpCodes.Sub);
+			il.Emit(OpCodes.Conv_U1);
+			il.Emit(OpCodes.Stelem_I1);
+		}
+
+		private static void EmitOutput(ILGenerator il, MethodInfo write)
+		{
+			il.Emit(OpCodes.Ldloc_0);
+			il.Emit(OpCodes.Ldloc_1);
+			il.Emit(OpCodes.Ldelem_U1);
+			il.EmitCall(OpCodes.Call, write, null);
+		}
+
+		private static void EmitInput(ILGenerator il, MethodInfo strLen, MethodInfo strChars, MethodInfo substring)
+		{
+			il.Emit(OpCodes.Ldloc_2);
+			il.EmitCall(OpCodes.Callvirt, strLen, null);
+			il.Emit(OpCodes.Ldc_I4_0);
+			il.Emit(OpCodes.Cgt);
+			var lbl = il.DefineLabel();
+			il.Emit(OpCodes.Brfalse_S, lbl);
+			il.Emit(OpCodes.Ldloc_0);
+			il.Emit(OpCodes.Ldloc_1);
+			il.Emit(OpCodes.Ldloc_2);
+			il.Emit(OpCodes.Ldc_I4_0);
+			il.EmitCall(OpCodes.Callvirt, strChars, null);
+			il.Emit(OpCodes.Conv_U1);
+			il.Emit(OpCodes.Stelem_I1);
+			il.Emit(OpCodes.Ldloc_2);
+			il.Emit(OpCodes.Ldc_I4_1);
+			il.EmitCall(OpCodes.Callvirt, substring, null);
+			il.Emit(OpCodes.Stloc_2);
+			il.MarkLabel(lbl);
 		}
 	}
 }
